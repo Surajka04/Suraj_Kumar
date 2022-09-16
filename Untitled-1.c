055a6e15114ac1b11b750c42f5e4e642fc673ade
Surajka04 <113572543+Surajka04@users.noreply.github.com>
2022-09-16 17:07:16 +0530

    C command for Hello World

---
+++ Untitled-1.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+int main()
+{
+   printf("Hello, World! \n");
+   printf("Hello, World! \n");
+   printf("Hello, World! \n");
+   printf("Hello, World! \n");
+   printf("Hello, World! \n");
+   printf("Hello, World! \n");
+   printf("Hello, World! \n");
+
+   return 0;
+}
